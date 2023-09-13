bc7c10cbc5ef830e6dedab508f7cd900cdb3321c
ZeyuLi <zli42391@usc.edu>
2023-09-13 16:58:17 -0700

    Setting up CLI

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import org.jline.reader.*;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Terminal terminal = TerminalBuilder.builder().system(true).build();
+            LineReader lineReader = LineReaderBuilder.builder().terminal(terminal).build();
+
+            System.out.println("Welcome to My Database App!");
+            System.out.println("Type 'exit' to quit.");
+
+            String prompt = "zeyDB> ";
+            String line;
+
+            while (true) {
+                try {
+                    line = lineReader.readLine(prompt);
+                    if (line == null || line.equalsIgnoreCase("exit")) {
+                        System.out.println("Exiting...");
+                        break;
+                    }
+
+                    // Process the user's query here.
+//                    parseQuery(line);
+                } catch (UserInterruptException e) {
+                    // Handle Ctrl+C or other user interruptions if needed.
+                    System.out.println("Interrupted.");
+                }
+            }
+        } catch (EndOfFileException | IOException e) {
+            System.out.println("");
+        }
+    }
+
+
+
+
+}
